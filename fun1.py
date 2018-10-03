dcda11c54bbfacc128f3808444f9b64410f9d98d
sambapython <sambapython@gmail.com>
2018-10-03 08:36:15 +0530

    2018-10-03 function memory management and return statement

---
+++ fun1.py
@@ -0,0 +1,11 @@
+x1=1234
+x2=5678
+def fun(x3,x4,y1):
+    print "x1=%s,x2=%s,x3=%s,x4=%s,y1=%s"%(x1,x2,x3,x4,y1)
+    z1=x1+y1
+    z2=x3+x4
+    print "z1=%s,z2=%s"%(z1,z2)
+print "other statments in program"
+fun(1000,2000,3000)
+print "program ended"
+    
---
+++ fun2.py
@@ -0,0 +1,7 @@
+print "program started"
+def fun(x1,x2):
+    print "inside fun: x1=%s, x2=%s"%(x1,x2)
+print "other statement in program"
+fun(1000,2000)
+print "outside fun: x1=%s, x2=%s"%(x1,x2)
+print "program ended"
